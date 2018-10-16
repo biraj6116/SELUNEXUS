715c24916345c30026770a2694506a4989378f55
biraj6116 <35614423+biraj6116@users.noreply.github.com>
2018-10-15 21:30:31 -0500

    test

---
+++ Services/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Services.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage ="Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string Address { get; set; }
+
+        [Compare("Password", ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Phonenumber is required.")]
+        public int Phonenumber { get; set; }
+
+        public ICollection<Service> Services { get; set; }
+
+
+
+    }
+
+}
